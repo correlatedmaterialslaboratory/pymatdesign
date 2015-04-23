6f2e445bc481d4ad3da37886ff5209784d6778bf
Chuck-Hou Yee <chuckyee@physics.rutgers.edu>
2015-04-23 19:06:57 -0400

    Add pymatgen as dependency.

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
-setup(name = 'pymatdesign',
-      version = '0.1',
-      description = 'Python Materials Design',
-      url = 'http://github.com/correlatedmaterialslaboratory/pymatdesign',
-      author = 'Chuck-Hou Yee',
-      author_email = 'chuckyee@physics.rutgers.edu',
-      license = 'MIT',
-      packages = ['pymatdesign'],
-      zip_safe = False)
+setup(
+    name = 'pymatdesign',
+    version = '0.1',
+    description = 'Python Materials Design',
+    url = 'http://github.com/correlatedmaterialslaboratory/pymatdesign',
+    author = 'Chuck-Hou Yee',
+    author_email = 'chuckyee@physics.rutgers.edu',
+    license = 'MIT',
+    packages = ['pymatdesign'],
+    install_requires = ['pymatgen']
+    )
