b8b70f65d657dd747437861478a885ed3ef9e5ed
Chuck-Hou Yee <chuckyee@physics.rutgers.edu>
2015-04-23 22:09:09 -0400

    Automatically import generate_compositions_by_oxidation

--- pymatdesign/__init__.py
+++ pymatdesign/__init__.py
@@ -3,3 +3,6 @@ from __future__ import unicode_literals
 __author__ = ", ".join(["Chuck-Hou Yee"])
 __date__ = "Apr 23 2015"
 __version__ = "0.0.1"
+
+
+from design import generate_compositions_by_oxidation
