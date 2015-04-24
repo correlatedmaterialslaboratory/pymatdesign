22ae4565e7eadaf116159e8df3c471ee0cb54e75
Chuck-Hou Yee <chuckyee@physics.rutgers.edu>
2015-04-23 22:07:40 -0400

    Add long readme description.

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
+
+def open_readme():
+    with open("README.md") as f:
+        text = f.read()
+    return text
+
+
 setup(
     name = 'pymatdesign',
     version = '0.1',
     description = 'Python Materials Design',
+    long_description = open_readme(),
     url = 'http://github.com/correlatedmaterialslaboratory/pymatdesign',
     author = 'Chuck-Hou Yee',
     author_email = 'chuckyee@physics.rutgers.edu',
